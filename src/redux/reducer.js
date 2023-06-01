cc6089477b4f718693312d480b5820a8a37f8514
mauro fallico <mauro.fallico@Barracas.com.ar>
2023-06-01 17:45:17 -0300

    01/06

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,20 +1,30 @@
-import { ADD_FAV, REMOVE_FAV } from "./actions"
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./actions"
 
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
 }
 
 const reducer = (state=initialState, action) => {
     switch (action.type){
         default:
             return state
+
         case ADD_FAV:
-            return {...state, myFavorites: [...state.myFavorites, action.payload]}  
+            return {...state, myFavorites: [...state.myFavorites, action.payload], 
+                              allCharacters: [...state.allCharacters, action.payload]}  
+
         case REMOVE_FAV:
             const removeId = parseInt(action.payload)
-            const filtered = state.myFavorites.filter((char => char.id !== removeId))
-            return {...state, myFavorites: filtered }
+            const filterRemove = state.myFavorites.filter((char => char.id !== removeId))
+            return {...state, myFavorites: filterRemove }
+
+        case FILTER:
+            const filterGender = state.myFavorites.filter((char => char.gender === action.payload))
+            return {...state, myFavorites: [filterGender],
+                              allCharacters: [...state.allCharacters, action.payload]}
+
     }
 }
 
