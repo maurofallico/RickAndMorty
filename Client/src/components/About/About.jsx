5206472ed9ede8b1b25f00f0c4b1ed482787e886
mauro <maurofallico@gmail.com>
2023-10-19 21:37:05 -0300

    responsive

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -6,11 +6,10 @@ export default function About () {
         <div>
             <Nav />
             <img src="./profile.jpg" alt="Mauro" className={styled.profile}/>
-    <p className = {styled.texto}>Hello there! I'm Mauro, and I'm a FullStack Web Developer. </p>
-    <p className = {styled.texto}>I created this project to practice new technologies and further my knoweledge.</p>
-    <p className = {styled.texto}>Any feedback will be appreciated.</p>
-    <br></br>
-    <p className={styled.texto}>
+            <div className = {styled.container}>
+    <p className = {styled.texto}><br></br>Hello there! I'm Mauro, and I'm a FullStack Web Developer. <br></br><br></br>
+    I created this project to practice new technologies, improve my skills, and further my knowledge. <br></br><br></br>
+    Any feedback will be appreciated. <br></br><br></br>
         You can reach me out through{" "}
         <a className = {styled.link} href="https://www.linkedin.com/in/maurofallico/" target="_blank" rel="noopener noreferrer">
           Linkedin
@@ -19,8 +18,9 @@ export default function About () {
         <a className = {styled.link} href="https://github.com/maurofallico" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
-      </p>
-    
+        <br></br><br></br>
+        </p>
+      </div>
     </div>
     )
 }
