46e0c08b8b0c2046106f900878067d31e39fabc5
maurofallico <maurofallico@gmail.com>
2023-12-28 01:54:01 -0300

    back-deploy

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -7,7 +7,7 @@ export const GET_CHARACTERS = "GET_GHARACTERS";
 
 export const getCharacters = (name) =>{
   return async function (dispatch){
-    const response = await axios.get(`http://localhost:3001/rickandmorty/characters?name=${name}`);
+    const response = await axios.get(`http://rickandmorty-4d7y.onrender.com/rickandmorty/characters?name=${name}`);
 return dispatch ({
     type: GET_CHARACTERS,
     payload: response.data
@@ -17,7 +17,7 @@ return dispatch ({
 
 
 export const addFav = (character) => {
-    const endpoint = `http://localhost:3001/rickandmorty/fav`;
+    const endpoint = `http://rickandmorty-4d7y.onrender.com/rickandmorty/fav`;
     return async (dispatch) => {
       const { data } = await axios.post(endpoint, character);
       return dispatch({
@@ -29,7 +29,7 @@ export const addFav = (character) => {
 
 export const removeFav = (id) => {
   try {
-    const endpoint = `http://localhost:3001/rickandmorty/fav/` + id;
+    const endpoint = `http://rickandmorty-4d7y.onrender.com/rickandmorty/fav` + id;
     return async (dispatch) => {
       const { data } = await axios.delete(endpoint)
         return dispatch({
