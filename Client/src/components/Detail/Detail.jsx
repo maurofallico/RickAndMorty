9ba38d6d1154a28bcdac7b5c4282720e70a3c12e
maurofallico <maurofallico@gmail.com>
2024-01-03 12:37:17 -0300

    details

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -39,10 +39,22 @@ export default function Detail (){
       </div>) : ( <div className = {styled.formContainer}>
         <div className = {styled.texto}>
             <h1 className = {styled.titulo}>{character.name}</h1>
-            <p className = {styled.dato}><strong>STATUS: </strong>{character.status}</p>
-            <p className = {styled.dato}><strong>GENDER: </strong>{character.gender}</p>
-            <p className = {styled.dato}><strong>SPECIE: </strong>{character.species}</p>
-            <p className = {styled.dato}><strong>ORIGIN: </strong>{character.origin.name}</p>
+            <div className={styled.datos}>
+            <p className = {styled.dato}><strong>STATUS</strong></p>
+            <p className = {styled.dato}>{character.status}</p>
+            </div>
+            <div className={styled.datos}>
+            <p className = {styled.dato}><strong>GENDER</strong></p>
+            <p className = {styled.dato}>{character.gender}</p>
+            </div>
+            <div className={styled.datos}>
+            <p className = {styled.dato}><strong>SPECIE</strong></p>
+            <p className = {styled.dato}>{character.species}</p>
+            </div>
+            <div className={styled.datos}>
+            <p className = {styled.dato}><strong>ORIGIN</strong></p>
+            <p className = {styled.dato}>{character.origin.name}</p>
+            </div>
             <NavLink to="/home" className={styled.button}>Back</NavLink>
         </div>
         <div className = {styled.imagen}>
