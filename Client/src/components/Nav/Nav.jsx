951b1fa63b499a4d422fccdce6e4d639af0856ae
maurofallico <maurofallico@gmail.com>
2024-01-03 01:07:05 -0300

    responsive

--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -7,7 +7,26 @@ import SearchBar from '../SearchBar/SearchBar'
 export default function Nav ({searchCharacter}) {
 
     const [select, setSelect] = useState('home')
+    const [windowWidth, setWindowWidth] = useState(0);
     const location = useLocation();
+
+    const fav = windowWidth < 420 ? ("Favs") : ("Favorites")
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+          setWindowWidth(window.innerWidth);
+    
+          const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+          };
+    
+          window.addEventListener('resize', handleResize);
+    
+          return () => {
+            window.removeEventListener('resize', handleResize);
+          };
+        }
+      }, []);
 
     useEffect(() => {
         switch (location.pathname){
@@ -26,16 +45,17 @@ export default function Nav ({searchCharacter}) {
         return (
                 <div className= {styled.container}>
                     <div className = {styled.buttonContainer}>
-                    <NavLink to="/home" className={select === 'home' ? styled.selected : styled.button}>Home </NavLink>
-                    <NavLink to="/favorites" className={select === 'favorites' ? styled.selected : styled.button}>Favorites </NavLink>
-                    <NavLink to="/about" className={select === 'about' ? styled.selected : styled.button}>About </NavLink>
+                    <NavLink to="/home" className={select === 'home' ? styled.selected : styled.button}>Home</NavLink>
+                    <NavLink to="/favorites" className={select === 'favorites' ? styled.selected : styled.button}>{fav}</NavLink>
+                    <NavLink to="/about" className={select === 'about' ? styled.selected : styled.button}>About</NavLink>
                     </div>
-                    <div className={styled.logoutContainer}>
-                    <NavLink to ="/" className={styled.button}>Logout</NavLink>
-                    </div>
+                    
                     
                     <div className = {styled.searchContainer}>
                     {location.pathname === '/home' && <SearchBar className = {styled.busqueda} searchCharacter={searchCharacter} />}
+                    </div>
+                    <div className={styled.logoutContainer}>
+                    <NavLink to ="/" className={styled.logoutButton}>Logout</NavLink>
                     </div>
                 </div>
         )
